3818a9d28b2ddf24610c2d4e6559162b9912cde1
Wesley Souza <wesleyadriann@gmail.com>
2019-09-15 00:57:34 -0300

    Adds axios in dependencies and get countries

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState }  from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [loading, setLoading] = useState(true)
+  const [countries, setCountries] = useState([])
+ 
+  useEffect(() => {
+    axios.get("https://restcountries.eu/rest/v2/all")
+    .then(data => {
+      setCountries(data.data);
+      setLoading(false);
+    });
+  }, [])
+
   return (
     <div>
-      
+      {loading ? 
+        <p>Carregando</p>
+        :
+        <select>
+          {countries.map(countrie => {
+            return (
+              <option>
+                {countrie.name}
+              </option>
+            )
+          })}
+        </select>
+      }
     </div>
   );
 }
